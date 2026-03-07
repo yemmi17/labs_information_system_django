af1455ec9e00629e40406cf2dc56476af7ce0239
Yemmi <nikitabrworkjob>
2026-03-07 13:46:25 +0300

    develop: admin-panel: add admin auth + add home page

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Product, Order, OrderItem
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "total_purchases", "current_balance", "credit_limit", "current_debt")
+    search_fields = ("name",)
+    list_filter = ("credit_limit",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock")
+    search_fields = ("name",)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "client", "sale_type", "created_at", "total_amount")
+    list_filter = ("sale_type", "created_at")
+    search_fields = ("client__name",)
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity", "price")
+    search_fields = ("order__id", "product__name")
+
